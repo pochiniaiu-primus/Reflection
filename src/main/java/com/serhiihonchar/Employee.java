e28bd12a3218c3ef0da938a48187a5e5092772a9
Serhii Honchar <serhii.honchar.java@gmail.com>
2021-10-31 01:52:23 +0300

    add 4 methods

---
+++ src/main/java/com/serhiihonchar/Employee.java
@@ -0,0 +1,47 @@
+package com.serhiihonchar;
+
+public class Employee {
+    public int id;
+    public String name;
+    public String department;
+    private double salary = 550;
+    private boolean isVaccinated = true;
+
+    public Employee() {
+
+    }
+
+    public Employee(int id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    private void changeDepartment(String newDepartment) {
+        department = newDepartment;
+        System.out.println("New department is: " + department);
+    }
+
+    public void increaseSalary() {
+        salary *= 2;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                ", isVaccinated=" + isVaccinated +
+                '}';
+    }
+}
---
+++ src/main/java/com/serhiihonchar/Main.java
@@ -0,0 +1,90 @@
+package com.serhiihonchar;
+
+import java.lang.reflect.*;
+import java.util.Arrays;
+
+public class Main {
+    //Метод принимает класс и возвращает созданный объект этого класса
+    static Employee methodReturnObject(Employee employee) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class employeeClass = Employee.class;
+        Constructor<Employee> constructor2 =
+                employeeClass.getConstructor(int.class, String.class, String.class);
+        employee = constructor2.newInstance(1, "Serhii", "IT");
+        return employee;
+    }
+
+    //Метод принимает object и вызывает у него все методы без параметров
+    static void returnAllMethodsWithoutParameters(Object obj) {
+        obj = Object.class;
+        Method[] someMethod2 = ((Class) obj).getDeclaredMethods();
+        for (Method method : someMethod2) {
+            if (Arrays.toString(method.getParameterTypes()).equals("[]")) {
+                System.out.println(method);
+            }
+        }
+    }
+
+    //Метод принимает object и выводит на экран все сигнатуры методов в который есть final
+    static void printMethodsWithFinal(Object obj) {//need refactor
+        obj = Object.class;
+        int mods = ((Class) obj).getModifiers();
+        Method[] someMethod3 = ((Class) obj).getDeclaredMethods();
+        for (Method method : someMethod3) {
+            if (Modifier.isFinal(mods))
+                System.out.println(method);
+        }
+    }
+
+    //Метод принимает Class и выводит все не публичные методы этого класса
+    static void printNotPublicMethods(Class clazz) {
+        clazz = Class.class;
+        Method[] someMethod4 = clazz.getDeclaredMethods();
+        for (int i = 0; i < someMethod4.length; i++) {
+            int modifiers = someMethod4[i].getModifiers();
+            if (!Modifier.isPublic(modifiers)) {
+                System.out.println(someMethod4[i]);
+            }
+        }
+    }
+
+    static void printParentClassesAndInterfaces(Class clazz) {
+
+    }
+
+    //Метод принимает объект и меняет всего его приватные поля на их нулевые значение (null, 0, false etc)+
+    static void changeValues(Employee employee) throws NoSuchFieldException, IllegalAccessException {
+        employee = new Employee(10, "Serhii", "IT");
+        Class employeeClass = employee.getClass();
+        Field field1 = employeeClass.getDeclaredField("salary");
+        Field field2 = employeeClass.getDeclaredField("isVaccinated");
+        field1.setAccessible(true);
+        field2.setAccessible(true);
+//        double salaryValue = (Double) field1.get(employee);
+//        boolean isVaccinatedValue =  (Boolean)field2.get(employee);
+        field1.set(employee, 0);
+        field2.set(employee, false);
+        System.out.println(employee);
+    }
+
+    public static void main(String[] args) throws
+            InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, NoSuchFieldException {
+        Employee employee = new Employee();
+        System.out.println(methodReturnObject(employee));
+        System.out.println("==================================================================================");
+        Object o1 = new Object();
+        returnAllMethodsWithoutParameters(o1);
+        System.out.println("==================================================================================");
+        Object o2 = new Object();
+        printMethodsWithFinal(o2);
+        System.out.println("==================================================================================");
+        Class c1 = Employee.class;
+        printNotPublicMethods(c1);
+//        System.out.println("==================================================================================");
+//        Class c2 = Employee.class;
+//        method5(c2);
+        System.out.println("=======================================");
+        Employee emp= new Employee();
+        changeValues(emp);
+    }
+
+}
